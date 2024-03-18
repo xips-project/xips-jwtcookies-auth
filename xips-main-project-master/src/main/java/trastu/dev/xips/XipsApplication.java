8423520421a20504c786c963cca9a7396bee1898
Alex <7701272+afcasco@users.noreply.github.com>
2024-03-18 13:41:04 +0100

    add test user with username test and password test if there are no users in the db

--- xips-main-project-master/src/main/java/trastu/dev/xips/XipsApplication.java
+++ xips-main-project-master/src/main/java/trastu/dev/xips/XipsApplication.java
@@ -1,19 +1,43 @@
 package trastu.dev.xips;
 
 import io.jsonwebtoken.security.Password;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import trastu.dev.xips.entities.User;
+import trastu.dev.xips.entities.UserProfile;
+import trastu.dev.xips.repositories.UserRepository;
+import trastu.dev.xips.services.UserService;
 
 @SpringBootApplication
-public class XipsApplication {
+@RequiredArgsConstructor
+public class XipsApplication implements CommandLineRunner {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(XipsApplication.class, args);
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.count() == 0) {
+
+            User user = User.builder()
+                    .username("test")
+                    .password(passwordEncoder.encode("test"))
+                    .userProfile(new UserProfile())
+                    .build();
+
+            userRepository.save(user);
+        }
+    }
 }
